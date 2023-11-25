5d40f3e92f90f438286d459868fac83971888fa0
Pak <pakapongm@gmail.com>
2023-11-24 19:09:29 -0800

    added a getShape function to each class function to return shapetype

--- examples/lib/shapes.js
+++ examples/lib/shapes.js
@@ -5,48 +5,44 @@ class Shape {
     this.square = square;
 }
 
-  shapeType(input) {
-    if (input === "Circle") {
-      return `circle cx="150" cy="100" r="80"`;
-    } else if (input === "Triangle") {
-      return `polygon points="150, 18 244, 182 56, 182"`;
-    } else if (input === "Square") {
-      return `rect x="90" y="40" width="120" height="120"`;
-    } else {
-      console.log("nothing??");
-      return "";
-    }
-  }
+//   shapeType(input) {
+//     if (input === "Circle") {
+//       return `circle cx="150" cy="100" r="80"`;
+//     } else if (input === "Triangle") {
+//       return `polygon points="150, 18 244, 182 56, 182"`;
+//     } else if (input === "Square") {
+//       return `rect x="90" y="40" width="120" height="120"`;
+//     } else {
+//       console.log("nothing??");
+//       return "";
+//     }
+//   }
 }
 function Circle(cx, cy, r) {
     Shape.call(this, cx, cy, r);
     this.circle = this.circle;
+    this.getShape = function (){
         return `circle cx="150" cy="100" r="80"`;
+    }
 }
 const newCircle = new Circle('150','100','80')
 
 function Triangle(point) {
     Shape.call(this, point);
     this.triangle = triangle;
-    return `polygon points="150, 18 244, 182 56, 182"`
+    this.getShape = function (){
+        return `polygon points="150, 18 244, 182 56, 182"`
+    }
   }
   const newTriangle = new Triangle("150, 18 244, 182 56, 182")
 
   function Square(x, y, width, height) {
     Shape.call(this, x, y, width, height);
     this.square = square;
-    return `x="90" y="40" width="120" height="120"`
+    this.getShape = function (){
+        return `x="90" y="40" width="120" height="120"`
+    }
   }
   const newSquare = new Square("90","40","120","120")
   
-  Dog.prototype.bark = function () {
-    console.log('Woof!');
-  };
-  
-  Dog.prototype.bark = function () {
-    console.log('Woof!');
-  };
-
-
-
 module.exports = Shape;
