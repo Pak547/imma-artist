0cc8750fedaf274f592068adb953d4c39ddc4baf
Pak <pakapongm@gmail.com>
2023-11-27 18:35:21 -0800

    went to officehours and learned to make each shape a class

--- examples/lib/generateSVG.js
+++ examples/lib/generateSVG.js
@@ -1,12 +1,9 @@
-const colorChoice = require("./colors")
-const shapeType = require("./shapes")
-const inquirer = require("inquirer")
-const colorChoice = require("./colors");
-const shapeType = require("./shapes");
+const shapeColor = require('./colors')
+const Shape = require('./shapes')
 
 function generateSVG(response) {
   return `<svg version="1.1" width="400" height="300" xmlns="http://www.w3.org/2000/svg">
-<${shapeType(response.shape)} fill="${colorChoice(response.color)}" />
+<${Shape(response.shape)} fill="${colorChoice(response.color)}" />
 <text x="150" y="155" font-size="60" text-anchor="middle" fill="${colorChoice(response.shapeColor)}">${response.text}</text>
 </svg>`;
 }
--- examples/lib/shapes.js
+++ examples/lib/shapes.js
@@ -1,50 +1,66 @@
 class Shape {
-  constructor(circle, triangle, square) {
-    this.circle = circle;
-    this.triangle = triangle;
-    this.square = square;
-}
-
-//   shapeType(input) {
-//     if (input === "Circle") {
-//       return `circle cx="150" cy="100" r="80"`;
-//     } else if (input === "Triangle") {
-//       return `polygon points="150, 18 244, 182 56, 182"`;
-//     } else if (input === "Square") {
-//       return `rect x="90" y="40" width="120" height="120"`;
-//     } else {
-//       console.log("nothing??");
-//       return "";
-//     }
-//   }
-}
-function Circle(cx, cy, r) {
-  this.cx = cx;
-  this.cy = cy;
-  this.r = r;
-    this.getShape = function (){
-        return `circle cx="150" cy="100" r="80"`;
-    }
-}
-const newCircle = new Circle('150','100','80')
-
-function Triangle(point) {
-    this.point = point;
-    this.getShape = function (){
-        return `polygon points="150, 18 244, 182 56, 182"`
+  constructor(type) {
+    this.type = type;
+  }
+  getShape() {
+    switch (this.type) {
+      case 'circle':
+        return this.getCircle(this.cx, this.cy, this.r);
+      case 'triangle':
+        return this.getTriangle(this.point);
+      case 'square':
+        return this.getSquare(this.x, this.y, this.width, this.height);
+      default:
+        throw new Error('Invalid shape type');
     }
   }
-  const newTriangle = new Triangle("150, 18 244, 182 56, 182")
+  getCircle(cx, cy, r) {
+    this.cx = cx;
+    this.cy = cy;
+    this.r = r;
+    return `circle cx="150" cy="100" r="80"`;
+  }
 
-  function Square(x, y, width, height) {
+  getTriangle(point) {
+    this.point = point;
+    return `polygon points="150, 18 244, 182 56, 182"`
+  }
+  getSquare(x, y, width, height) {
     this.x = x;
     this.y = y;
     this.width = width;
     this.height = height
-    this.getShape = function (){
-        return `x="90" y="40" width="120" height="120"`
-    }
+    return `x="90" y="40" width="120" height="120"`
   }
-  const newSquare = new Square("90","40","120","120")
-  
+}
+class Circle {
+  constructor(cx, cy, r) {
+    this.cx = cx;
+    this.cy = cy;
+    this.r = r;
+    return `circle cx="150" cy="100" r="80"`;
+  }
+}
+
+class Triangle {
+  constructor(point) {
+    this.point = point;
+    return `polygon points="150, 18 244, 182 56, 182"`
+  }
+}
+
+class Square {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    return `x="90" y="40" width="120" height="120"`
+
+  }
+}
+
 module.exports = Shape;
+module.exports = Triangle;
+module.exports = Square;
+module.exports = Circle;
